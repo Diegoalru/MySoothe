fe4c3f1479c4beb4c4b5c4529cae80908cd27ca8
Diego Alonso Rubí Salas <43016665+Diegoalru@users.noreply.github.com>
2024-08-06 13:01:42 -0600

    Improve dataclass and list naming.

--- app/src/main/java/com/codelab/basiclayouts/data/Data.kt
+++ app/src/main/java/com/codelab/basiclayouts/data/Data.kt
@@ -4,27 +4,31 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.codelab.basiclayouts.R
 
-class Data {
+data class ImageTextPair(
+    @DrawableRes val imageResId:Int,
+    @StringRes val textResId: Int
+)
 
-    val alignYourBodyData = listOf(
-        R.drawable.ab1_inversions to R.string.ab1_inversions,
-        R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,R.drawable.ab3_stretching to R.string.ab3_stretching,
-        R.drawable.ab4_tabata to R.string.ab4_tabata,
-        R.drawable.ab5_hiit to R.string.ab5_hiit,
-        R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
-    ).map { DrawableStringPair(it.first, it.second) }
+private val alignYourBodyItems = listOf(
+    ImageTextPair(R.drawable.ab1_inversions, R.string.ab1_inversions),
+    ImageTextPair(R.drawable.ab2_quick_yoga, R.string.ab2_quick_yoga),
+    ImageTextPair(R.drawable.ab3_stretching, R.string.ab3_stretching),
+    ImageTextPair(R.drawable.ab4_tabata, R.string.ab4_tabata),
+    ImageTextPair(R.drawable.ab5_hiit, R.string.ab5_hiit),
+    ImageTextPair(R.drawable.ab6_pre_natal_yoga,R.string.ab6_pre_natal_yoga)
+)
 
-    val favoriteCollectionsData = listOf(
-        R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
-        R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
-        R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
-        R.drawable.fc4_self_massage to R.string.fc4_self_massage,
-        R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
-        R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
-    ).map { DrawableStringPair(it.first, it.second) }
+val alignYourBodyData: List<ImageTextPair>
+    get() = alignYourBodyItems
 
-    data class DrawableStringPair(
-        @DrawableRes val drawable: Int,
-        @StringRes val text: Int
-    )
-}
+private val favoriteCollectionItems = listOf(
+    ImageTextPair(R.drawable.fc1_short_mantras, R.string.fc1_short_mantras),
+    ImageTextPair(R.drawable.fc2_nature_meditations, R.string.fc2_nature_meditations),
+    ImageTextPair(R.drawable.fc3_stress_and_anxiety, R.string.fc3_stress_and_anxiety),
+    ImageTextPair(R.drawable.fc4_self_massage, R.string.fc4_self_massage),
+    ImageTextPair(R.drawable.fc5_overwhelmed, R.string.fc5_overwhelmed),
+    ImageTextPair(R.drawable.fc6_nightly_wind_down, R.string.fc6_nightly_wind_down)
+)
+
+val favoriteCollectionsData: List<ImageTextPair>
+    get() = favoriteCollectionItems
